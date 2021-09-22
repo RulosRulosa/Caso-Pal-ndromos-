3d553ddff446e8b0c54617872dd26d13510a0946
Nacho <i.vallejos01@ufromail.cl>
2021-09-22 10:14:34 -0300

    -esPalindromo

---
+++ CasoPalindromos/src/main/java/CasoPalindromos.java
@@ -0,0 +1,11 @@
+public class CasoPalindromos {
+
+    public static boolean esPalindromo(String cadena) {
+        cadena = cadena.toLowerCase().replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o")
+                .replace("ú", "u").replace(" ", "").replace(".", "").replace(",", "");
+
+        String invertida = new StringBuilder(cadena).reverse().toString();
+
+        return invertida.equals(cadena);
+    }
+}
